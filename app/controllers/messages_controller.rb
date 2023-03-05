e60842704f6757c5574d814d1ba81db91b8206fb
kenzamu <jbantian553@gmail.com>
2023-03-05 14:50:43 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(room_params)
+    if   @message.save
+      redirect_to room_message_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:message).permit(:contant).marge(user_id: current_user.id)
+  end
+
 end
